4b6d0e12a52ab2f4c1a11a4ee05b2ad4d6317f71
Carys Mills <mills.carys@gmail.com>
2015-10-22 15:41:57 -0400

    adding map start to JS and SCSS

--- scripts/main.js
+++ scripts/main.js
@@ -5,6 +5,8 @@ activity.url = "https://api.foursquare.com/v2/venues/explore";
 activity.client = "LXPSZXFCGFE1VMU33P4CYHFZXNLZDA3GIPM05SJI2AS1W20H";
 activity.secret = "PDZOVDI2YNL1IJ1NLSPZ0MJ3VXFTHY2DDF0LMWN2HC01FENV";
 activity.version = "2013081520";
+activity.access = "pk.eyJ1IjoiY2FyeXMiLCJhIjoiY2lmcnA0bDAxMG1yNHMybTB4cDFkMnEzMyJ9.4Z26iDuKWwLy8qs1MyTkDg";
+
 
 //initial ajax call
 activity.getChoice = function(userChoice) {
@@ -21,6 +23,7 @@ activity.getChoice = function(userChoice) {
     }
   }).then(function(res) {
       activity.displayResults(res.response.groups[0].items); //to get within the array info that we need. could move elsewhere
+      activity.plotMap();
   });
 };
 
@@ -35,16 +38,28 @@ activity.displayResults = function(venues){
     var review = $("<p>").text(value.tips[0].text); // ***I think this error is because some don't have this, causes not to show
     var website = $("<a>").attr("href", value.venue.url);
     var container = $("<div>").append(name, location, hours, rating, review, website);
-    // var info = $(".text").append(container);
     $("#text").append(container);
+  });
+};
+
+activity.getMap = function() {
+  L.mapbox.accessToken = activity.access;
+  activity.map = L.mapbox.map('map', 'carys.nn6p55nf')
+      .setView([43.677, -79.436], 12);
+};
+
+activity.plotMap = function(){
+  $.each(venues, function(i, value) {
+      L.marker([value.venue.location.lat,value.venue.location.lng]).addTo(activity.map);
   });
 };
 
 // everything to run on doc ready
 activity.init = function() {
+  activity.getMap(); 
 	activity.getChoice("bowling"),
   $("#activity").on("change", function() {
-      var quirky = $(this).val(); // ***this variable was called activity and i think it was confusing because of global variable
+      var quirky = $(this).val();
       $("#text").empty();
       activity.getChoice(quirky);
   });
